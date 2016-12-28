8bd5b95c7abf0100d88e80513951cbb668cb6f4c
Ivan Popelyshev <ivan.popelyshev@gmail.coma>
2016-12-28 19:21:18 +0300

    utils

--- postgresql-async/src/main/kotlin/com/github/mauricio/async/db/postgresql/util/PasswordHelper.kt
+++ postgresql-async/src/main/kotlin/com/github/mauricio/async/db/postgresql/util/PasswordHelper.kt
@@ -21,55 +21,55 @@ import java.security.MessageDigest
 
 object PasswordHelper {
 
-  private final val Lookup = Array[Byte]('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f')
+    private final val Lookup = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f')
 
-  private fun bytesToHex( bytes : Array[Byte], hex : Array[Byte], offset : Int ) {
+    private fun bytesToHex(bytes: ByteArray, hex: ByteArray, offset: Int) {
 
-    var pos = offset
-    var i = 0
+        var pos = offset
+        var i = 0
 
-    while ( i < 16 ) {
-      val c = bytes(i) & 0xff
-      var j = c >> 4
-      hex(pos) = Lookup(j)
-      pos += 1
-      j = (c & 0xf)
+        while (i < 16) {
+            val c = bytes[i].toInt() and 0xff
+            var j = c.toInt() shr 4
+            hex[pos] = Lookup[j].toByte()
+            pos += 1
+            j = (c and 0xf)
 
-      hex(pos) = Lookup(j)
-      pos += 1
+            hex[pos] = Lookup[j].toByte()
+            pos += 1
 
-      i += 1
+            i += 1
+        }
+
     }
 
-  }
+    fun encode(userText: String, passwordText: String, salt: ByteArray, charset: Charset): ByteArray {
+        val user = userText.toByteArray(charset)
+        val password = passwordText.toByteArray(charset)
 
-  fun encode( userText : String, passwordText : String, salt : Array[Byte], charset : Charset ) : Array[Byte] = {
-    val user = userText.getBytes(charset)
-    val password = passwordText.getBytes(charset)
+        val md = MessageDigest.getInstance("MD5")
 
-    val md = MessageDigest.getInstance("MD5")
+        md.update(password)
+        md.update(user)
 
-    md.update(password)
-    md.update(user)
+        val tempDigest = md.digest()
 
-    val tempDigest = md.digest()
+        val hexDigest = ByteArray(35)
 
-    val hexDigest = new Array[Byte](35)
+        bytesToHex(tempDigest, hexDigest, 0)
+        md.update(hexDigest, 0, 32)
+        md.update(salt)
 
-    bytesToHex(tempDigest, hexDigest, 0)
-    md.update(hexDigest, 0, 32)
-    md.update(salt)
+        val passDigest = md.digest()
 
-    val passDigest = md.digest()
+        bytesToHex(passDigest, hexDigest, 3)
 
-    bytesToHex(passDigest, hexDigest, 3)
+        hexDigest[0] = 'm'.toByte()
+        hexDigest[1] = 'd'.toByte()
+        hexDigest[2] = '5'.toByte()
 
-    hexDigest(0) = 'm'
-    hexDigest(1) = 'd'
-    hexDigest(2) = '5'
-
-    hexDigest
-  }
+        return hexDigest
+    }
 
 }
